ca4222c92f6a128fc1a7742c6678929847dbbf5e
BenAshani <benben.ashani@gmail.com>
2023-09-19 15:13:10 +0300

    HomePage small changes

--- src/components/HomePage/HomePageSecond.jsx
+++ src/components/HomePage/HomePageSecond.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 export default function HomePageSecond() {
   return (
@@ -6,23 +6,24 @@ export default function HomePageSecond() {
       <div className="about-us-second-wrapper">
         <div className="about-us-second-inner">
           <h1>#1 Ranking</h1>
+          <hr />
           <p>Ranked #1 on RIA Channel’s list of the Top 100 Wealth Managers</p>
         </div>
-        <hr />
         <div className="about-us-second-inner">
           <h1>Over $245 Billion</h1>
+          <hr />
           <p>
             Combined assets under management and advisement as of July 1, 2023
           </p>
         </div>
-        <hr />
         <div className="about-us-second-inner">
-          <h1>50 States and Abroad</h1>
+          <h1>The Whole World</h1>
+          <hr />
           <p>
             Providing financial peace of mind nationwide and across the globe
           </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
