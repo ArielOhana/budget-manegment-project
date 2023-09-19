1b86671b0cc5a3f34b9b02e7ee36b40f9617c48d
ArielOhana <140969225+ArielOhana@users.noreply.github.com>
2023-09-19 13:32:41 +0300

    Starting Personal Data

--- src/pages/PersonalData.jsx
+++ src/pages/PersonalData.jsx
@@ -1,10 +1,12 @@
 import { UserContext } from "../App";
 import { useContext } from "react";
+import InnerNavBar from "../components/InnerNavBar";
 const PersonalData = () => {
     const { user, setUser } = useContext(UserContext);
     console.log(user)
       return(
           <>
+          <InnerNavBar></InnerNavBar>
           <h1>Personal Data</h1>
           </>
     )
