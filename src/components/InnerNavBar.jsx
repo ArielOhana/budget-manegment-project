e4555e9801a3f2dc44636d880f2d8c74ece45545
ArielOhana <140969225+ArielOhana@users.noreply.github.com>
2023-09-19 13:32:41 +0300

    Starting Personal Data

---
+++ src/components/InnerNavBar.jsx
@@ -0,0 +1,33 @@
+import "../style/InnerNavBar.css";
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div className="navbar-container">
+      <ul className="navbar">
+        <div className="navbar-left">
+          <Link to="/">
+            <div className="navbar-left-img">
+              <img src="/images/navbar-icon.png" alt="Navbar icon" />
+            </div>
+            <div className="navbar-left-title">
+              <h2>BudgetBuddy</h2>
+            </div>
+          </Link>
+        </div>
+        <div className="navbar-right">
+          <li>
+            <NavLink to="/contact">Contact Us</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/register">Register</NavLink>
+          </li>
+        </div>
+      </ul>
+    </div>
+  );
+}
