56e271de231b1b5c2e7abcdfab67f985bc0ecd13
BenAshani <benben.ashani@gmail.com>
2023-09-21 12:01:03 +0300

    Pie Chart Added

--- src/pages/PersonalData.jsx
+++ src/pages/PersonalData.jsx
@@ -1,21 +1,39 @@
 import { UserContext } from "../App";
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import InnerNavBar from "../components/InnerNavBar";
 import { NavLink } from "react-router-dom";
+import { Chart } from "react-google-charts";
+import "../style/PersonalData.css";
+import InnerNavBar from "../components/InnerNavBar";
+
+export const data = [
+  ["Outcome", "Amount"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
+];
+
+let income = JSON.parse(localStorage.getItem("totalincome")) || [];
+
+export const options = {
+  title: `My Budget: ${income}`,
+  is3D: true,
+};
 
 const PersonalData = () => {
   const navigate = useNavigate();
-  const { user, setUser } = useContext(UserContext); 
+  const { user, setUser } = useContext(UserContext);
 
-  useEffect(() => {
-    if (!user.UserName) {
-      navigate("/error");
-    } 
-  }, [user, navigate]); 
+  // useEffect(() => {
+  //   if (!user.UserName) {
+  //     navigate("/error");
+  //   }
+  // }, [user, navigate]);
 
   return (
-    <>
+    <div className="personal-info-container">
       <InnerNavBar>
         <li>
           <NavLink to="/editprofile">Edit Profile</NavLink>
@@ -27,8 +45,16 @@ const PersonalData = () => {
           <NavLink to="/">Log Out</NavLink>
         </li>
       </InnerNavBar>
-      <h1>Personal Data</h1>
-    </>
+      <div className="budget-pie-chart">
+        <Chart
+          chartType="PieChart"
+          width="100%"
+          height="450px"
+          data={data}
+          options={options}
+        />
+      </div>
+    </div>
   );
 };
 
